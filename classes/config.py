29111268d7594ab86a40a7c43ce2814454af8a27
war <thomas@warwaris.at>
2017-08-07 06:10:56 +0200

    first commit

---
+++ classes/config.py
@@ -0,0 +1,135 @@
+import argparse
+import configparser
+
+class ConfigErrorException(Exception):
+    def __init__(self, message=None, errors=None):
+        super(ConfigErrorException, self).__init__(message)
+        self.errors = errors
+
+class CmdlineArgs(object):
+    def __init__(self,config,dump=None,log=False,output=None):
+        self.config = config
+        self.dump = dump
+        self.log = log
+        self.output = output
+
+class ConfigFile(object):
+    def __init__(self,filename):
+        self.filename = filename
+        self.config = configparser.ConfigParser()
+        files_read = self.config.read(self.filename)
+        if not files_read:
+            msg = "config file \"{}\" could not be processed".format(self.filename)
+            raise ConfigErrorException(msg)
+
+    def __repr__(self):
+        r = []
+        r.append(super(ConfigFile,self).__repr__())
+        r.append("filename: {}".format(self.filename))
+        return "\n".join(r)
+
+class DumpConfig(object):
+    def __init__(self,dump_name,config_file):
+        self.dump_name = dump_name
+        self.config_file = config_file
+
+        self.alt_names = []
+        try:
+            self.host = config_file.config.get(self.dump_name, 'Host')
+            self.cacertfile = config_file.config.get(self.dump_name, 'CacertFile', fallback=None)
+            self.cacertpath = config_file.config.get(self.dump_name, 'CacertPath', fallback=None)
+            self.alt_names_txt = config_file.config.get(self.dump_name, 'AltNames', fallback=None)
+            if self.alt_names_txt:
+                alt_names = self.alt_names_txt.split(',')
+                for n in alt_names:
+                    self.alt_names.append(n.strip())
+            self.admin_user = config_file.config.get(self.dump_name, 'AdminUser')
+            self.admin_pw = config_file.config.get(self.dump_name, 'AdminPw')
+            self.test_target_base_dn = config_file.config.get(self.dump_name, 'BaseDn')
+            self.test_target_filter = config_file.config.get(self.dump_name, 'Filter')
+            self.verifyssl = config_file.config.get(self.dump_name, 'VerifySSL', fallback=True)
+        except configparser.NoOptionError as e:
+            raise ConfigErrorException('a option is missing: '+str(e))
+
+
+
+    def __str__(self):
+        r = []
+        r.append("Host: {}".format(self.host))
+        r.append("Adminuser: {}".format(self.admin_user))
+        r.append("BaseDn: {}".format(self.test_target_base_dn))
+        r.append("Filter: {}".format(self.test_target_user_filter))
+        return ", ".join(r)
+
+
+    def __repr__(self):
+        r = []
+        r.append(super(TestConfig,self).__repr__())
+        r.append(self.__str__())
+        return "\n".join(r)
+
+
+
+class Config(object):
+
+    def __init__(self):
+        self.cmdline_args = None
+        self.config_file = None
+        self.dump_config = None
+        self.parser = None
+        self.args = None
+        self.selected_dump = None
+        self.__parser()
+        self.__parse_args()
+        self.__read_config_file()
+
+    def __parser(self):
+        self.parser = argparse.ArgumentParser(
+            description="Dump entries from a MSFT AD",
+            epilog="example: %(prog)s -c config.conf",
+            usage='%(prog)s [options]',
+            prog='dump'
+        )
+        self.parser.add_argument('-c', '--conf', dest='config', default='dump.conf',
+                    help='test config file')
+        self.parser.add_argument('-d', '--dump', dest='dump',
+            help='selects the dump in case more than one dump is defined in the config file')
+        self.parser.add_argument('-l', '--log', dest='log', action='store_true',
+            help='write a ldap log to the file "ldap.log"')
+        self.parser.add_argument('-o', '--output', dest='output', action='store_true',
+                                 help='output file', default='data.ldif')
+        self.parser.set_defaults(log=False)
+ 
+    def __parse_args(self):
+        self.args = self.parser.parse_args()
+        self.cmdline_args = CmdlineArgs(self.args.config, self.args.dump, self.args.log, self.args.output)
+
+    def __read_config_file(self):
+        self.config_file = ConfigFile(self.cmdline_args.config)
+        dumps_list = self.config_file.config.sections()
+        if not dumps_list:
+            raise ConfigErrorException("No dump was found in this config")
+        if self.cmdline_args.dump:
+            if self.cmdline_args.test in dumps_list:
+                self.selected_dump = self.cmdline_args.test
+            else:
+                raise ConfigErrorException("the dump \"{}\" was not found. Available dumps in this config: {}".format(
+                    self.cmdline_args.test, ",".join(dumps_list)))
+        else:
+            self.selected_dump = dumps_list[0]
+
+        self.dump_config = DumpConfig(self.selected_dump,self.config_file)
+
+    def __repr__(self):
+        r = []
+        r.append(super(Config,self).__repr__())
+        if not self.parser or not self.args:
+            r.append("Not initialized")
+        else:
+            r.append(str(self.args))
+            r.append(str(self.cmdline_args))
+            r.append(str(self.config_file))
+            r.append("selected dump: \"{}\"".format(self.selected_dump))
+            r.append(str(self.dump_config))
+        return "\n".join(r)
+
---
+++ classes/ldap.py
@@ -0,0 +1,173 @@
+import ldap3
+import ssl
+import re
+import ast
+import pprint
+import collections
+from ldap3.protocol.rfc2849 import search_response_to_ldif
+from string import ascii_lowercase
+
+PARTLEN = 2
+
+class LdapErrorException(Exception):
+    def __init__(self, message=None, errors=None):
+        super(LdapErrorException, self).__init__(message)
+        self.errors = errors
+
+class TestFailedException(Exception):
+    def __init__(self, message=None, errors=None):
+        super(TestFailedException, self).__init__(message)
+        self.errors = errors
+
+class TargetUser(object):
+    def __init__(self,entry):
+        self.entry = entry
+
+    def dn(self):
+        return str(self.entry.distinguishedName)
+
+
+class Ldap(object):
+    def __init__(self,config):
+        self.config = config
+
+    def run_dump(self):
+        self._test_connect()
+        self._test_naming_contexts()
+        self._test_search()
+        return
+
+    def _dump_user_data(self):
+        ordered_dict = collections.OrderedDict(self.target_user.__dict__)
+        pp = pprint.PrettyPrinter(indent=4)
+        pp.pprint(ordered_dict)
+        return
+
+
+    def _test_search(self):
+        count = 0
+        dn = self.config.dump_config.test_target_base_dn
+        f = self.config.dump_config.test_target_filter
+        all_entries = list()
+
+        output_file = open(self.config.cmdline_args.output, 'w')
+        output_file.close()
+
+        written_entries = 0
+        try:
+
+            for c1 in ascii_lowercase:
+                print("now : {}* seen: {} records".format(c1,count))
+                for c2 in ascii_lowercase:
+                    for c3 in ascii_lowercase:
+                        partkey = "{}{}{}".format(c1,c2,c3)
+                        filter = "(&{}(cn={}*))".format(f, partkey)
+                        #print(filter)
+                        result = self.connection.search(
+                            search_base=dn,
+                            search_filter=filter,
+                            dereference_aliases=ldap3.DEREF_NEVER,
+                            search_scope=ldap3.SUBTREE,
+                            attributes=['*']
+                        )
+                        cr = self.connection.result
+                        if (cr['result'] != 0):
+                            print ("WARNING: {} on {}".format(cr['description'],filter))
+
+                        for entry in self.connection.entries:
+                            edict = {
+                                'dn': entry.entry_dn,
+                                'raw_attributes': entry.entry_raw_attributes,
+                            }
+                            all_entries.append(edict)
+                            count +=1
+
+                    if count > written_entries:
+                        ldif = search_response_to_ldif(all_entries, all_base64=False, sort_order=None)
+                        ldif.pop()
+                        output_file = open(self.config.cmdline_args.output, 'a')
+                        for l in ldif:
+                            output_file.write(l + "\n")
+                        output_file.close()
+                        all_entries = list()
+                        written_entries = count
+
+                #ldif = entry.entry_to_ldif(stream=output_file)
+
+        except (ldap3.core.exceptions.LDAPInvalidFilterError,
+                ldap3.core.exceptions.LDAPObjectClassError) as e:
+            raise LdapErrorException(e)
+
+    def _test_connect(self):
+
+        try:
+            if self.config.dump_config.cacertfile or self.config.dump_config.cacertpath:
+                if self.config.dump_config.verifyssl:
+                    validate = ssl.CERT_REQUIRED
+                else:
+                    validate = ssl.CERT_NONE
+                tls = ldap3.Tls(
+                    validate=validate,
+                    ca_certs_file=self.config.dump_config.cacertfile,
+                    ca_certs_path=self.config.dump_config.cacertpath,
+                    valid_names=self.config.dump_config.alt_names
+                )
+            else:
+                tls = ldap3.Tls(validate=ssl.CERT_NONE)
+        except ldap3.core.exceptions.LDAPSSLConfigurationError as e:
+            raise LdapErrorException(e)
+
+        self.server = ldap3.Server(
+            host=self.config.dump_config.host,
+            port=636,
+            use_ssl=True,
+            tls=tls,
+            get_info=ldap3.ALL)
+        try:
+            self.connection = ldap3.Connection(
+                self.server,
+                self.config.dump_config.admin_user,
+                password=self.config.dump_config.admin_pw,
+                auto_bind=True,
+                client_strategy=ldap3.SYNC,
+                auto_range=True,
+                read_only=True,
+            )
+        except ldap3.core.exceptions.LDAPBindError as e:
+            raise LdapErrorException(e)
+        except ldap3.core.exceptions.LDAPSocketOpenError as e:
+            cert_error = self.__parse_certificate_error(e)
+            if cert_error:
+                raise LdapErrorException(cert_error)
+            raise LdapErrorException(e)
+
+        return
+
+    def __parse_certificate_error(self,exception):
+        message = str(exception)
+        match = re.match(r"socket ssl wrapping error: certificate {(.*)} .* in \[(.*)\]",
+                     message,
+                     re.IGNORECASE)
+        if match:
+            cert_str = "{" + match.group(1) + "}"
+            cert = ast.literal_eval(cert_str)
+            cert_subject= str(cert['subject'])
+            names_str = "[" + match.group(2) + "]"
+            names = ast.literal_eval(names_str)
+            names_joined = ",".join(names)
+            msg = "certificate error: subject does not match hostnames: {} subject: {}".format(names_joined,cert_subject)
+            return msg
+        else:
+            return False
+
+    def _test_naming_contexts(self):
+        if self.config.dump_config.test_target_base_dn not in self.server.info.naming_contexts:
+            r = ["the AD server does not have the naming context for {}".format(
+                self.config.dump_config.test_target_base_dn)]
+            r.append("supported naming contexts are:")
+            r = r + self.server.info.naming_contexts
+            msg = "\n".join(r)
+            raise TestFailedException(msg)
+        return
+
+
